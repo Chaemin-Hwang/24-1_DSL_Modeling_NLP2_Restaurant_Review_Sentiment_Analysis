05eb474634643cb64d2b45c2cee6ac338bbf75c7
Chaemin-Hwang <2021127020@yonsei.ac.kr>
2024-03-20 12:16:41 +0900

    app.py 변경

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request, session, redirect, url_for, flash, json
 import secrets
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
+
+# 메인 페이지 라우트
+@app.route('/')
+
+@app.route('/main_page')
+def main_page():
+    return render_template('main_page.html')
+
+app.run()
